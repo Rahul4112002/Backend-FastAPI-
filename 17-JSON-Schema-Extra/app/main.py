1bf639aed4f7142dd6ea92bab6c2eaf9d1d487d4
Rahul Chauhan <rahulchuahan4708@gmail.com>
2026-01-10 12:29:00 +0530

    17

--- 17-JSON-Schema-Extra/app/main.py
+++ 17-JSON-Schema-Extra/app/main.py
@@ -7,33 +7,33 @@ app = FastAPI()
 
 # Field-level Examples ka use karke - har field me alag example
 # ## Using Field-level Examples
-# class Product(BaseModel):
-#     name: str = Field(examples=["Moto E"])
-#     price: float = Field(examples=[23.56])
-#     stock: int | None = Field(default=None, examples=[43])
+class Product(BaseModel):
+    name: str = Field(examples=["Moto E"])
+    price: float = Field(examples=[23.56])
+    stock: int | None = Field(default=None, examples=[43])
 
 # @app.post("/products")
 # async def create_product(product: Product):
 #     return product
 
 ## Using Pydantic's json_schema_extra - poore model ka ek example
-class Product(BaseModel):
-  name: str
-  price: float
-  stock: int | None = None
+# class Product(BaseModel):
+#   name: str
+#   price: float
+#   stock: int | None = None
 
-  # Model config me complete example define karo
-  model_config = {
-    "json_schema_extra": {
-      "examples": [
-        {
-          "name": "Moto E",
-          "price": 34.56,
-          "stock": 45
-        }
-      ]
-    }
-  }
+#   # Model config me complete example define karo
+#   model_config = {
+#     "json_schema_extra": {
+#       "examples": [
+#         {
+#           "name": "Moto E",
+#           "price": 34.56,
+#           "stock": 45
+#         }
+#       ]
+#     }
+#   }
 
 # POST endpoint - product create karne ke liye
 @app.post("/products")
